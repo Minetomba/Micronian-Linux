5fbcbe48bd98bdd055bcd559ef840b6b0c7a4049
Minetomba <147551445+Minetomba@users.noreply.github.com>
2026-09-17 21:38:22 +0300

    3

--- README.md
+++ README.md
@@ -2,4 +2,16 @@
 **O**ne-**S**-tatic-**F**ile Linux is a minimalist distribution designed to be as small and efficient as possible.
 
 ## Installation
-Compile with "./compile.sh" and tell the bootloader to call the "osf" binary here.
+Compile with "./compile.sh", move into /sbin, and tell the kernel for it to be PID 1.
+To shutdown the system, send SIGUSR1 to PID 1 via syscall(SYS_kill, 1, 10), making sure only the root user can send that signal and be succesful.
+To configure the system, make a file named "/etc/initconf.txt" owned by root editable only by root, and type line by line the paths to the binaries to execute. For example, this is a very small yet functional initconf that spawns iwd, sshd, and a shell all as root:
+```conf
+/bin/iwd
+/sbin/sshd
+/bin/sh
+```
+By default, you should only use /bin and /sbin for binaries.
+All binaries in initconf.txt get launched as root, which means you might need to make a wrapper to switch users and launch a process based on file input (since arguments are not specified via initconf, initconf is just a list of paths to launch).
+
+## Dependencies
+- POSIX-compliant Linux
--- src/osf.c
+++ src/osf.c
@@ -1,31 +1,126 @@
+/* Includes */
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/prctl.h>
+#include <sys/reboot.h>
+#include <ctype.h>
+#include <sys/stat.h>
+#include <sys/mount.h>
+#include <dirent.h>
 #include <syscall.h>
 #include <unistd.h>
+#include <fcntl.h>
 
+/* Constants */
+#define PROCESSES_PATH "/etc/initconf.txt"
+#define MAX_COMMAND_LENGTH_CHARS 256
+#define PROCESS_TIMEOUT 5
+#define NOT_ROOT_ERROR "Error: Not running as root.\n"
+#define PRESYSINIT_MESSAGE "System launch!\n"
+
+/* Signal Handler */
+static volatile sig_atomic_t shutdown_requested = 0;
+static void on_shutdown(int sig) {
+	(void)sig;
+	shutdown_requested = 1;
+}
+
+/* Helper Functions */
+int npids() {
+	int n = 0;
+	struct dirent *e;
+	DIR *d = opendir("/proc");
+	while ((e=readdir(d))) {
+		if (isdigit((unsigned char)e->d_name[0]) != 0) {
+			n++;
+		}
+	}
+	closedir(d);
+	return n;
+}
 int isroot() {
 	return syscall(SYS_geteuid) == 0;
 }
+void initfs() {
+	/* /dev/console routing */
+	int fd = open("/dev/console", O_RDWR | O_NOCTTY);
+	if (fd >= 0) {
+		dup2(fd, 0);
+		dup2(fd, 1);
+		dup2(fd, 2);
+		if (fd > 2) {
+			close(fd);
+		}
+    }
 
-void init_fs() {
+	/* Directories */
 	syscall(SYS_mkdir, "/proc", 0555);
 	syscall(SYS_mount, "proc", "/proc", "proc", 0, NULL);
 	syscall(SYS_mkdir, "/sys", 0555);
 	syscall(SYS_mount, "sys", "/sys", "sys", 0, NULL);
 	return;
 }
+int initsys() {
+	/* Signal Handler Initiation */
+	struct sigaction sa = {0};
+	sa.sa_handler = on_shutdown;
+	sa.sa_flags = 0;
+	sigemptyset(&sa.sa_mask);
+	sigaction(SIGUSR1, &sa, NULL);
+	signal(SIGTERM, SIG_IGN);
 
-void shutdown() {
-	syscall(SYS_sync);
-	syscall(SYS_sync);
-	syscall(SYS_reboot, 0x4321fedc);
-	return;
+	/* Startup */
+	FILE *file = fopen(PROCESSES_PATH, "r");
+	char line[MAX_COMMAND_LENGTH_CHARS];
+	pid_t pid;
+	while (fgets(line, sizeof(line), file)) {
+		line[strcspn(line, "\n")] = 0;
+		printf("[ ... ] \"%s\"\n", line);
+		pid = fork();
+		if (pid == 0) {
+			char *args[] = {line, NULL};
+			execvp(line, args);
+			_exit(1);
+		} else {
+			printf("[ OK ] pid %d\n", pid);
+		}
+	}
+	fclose(file);
+	
+	/* Reaping */
+	prctl(PR_SET_CHILD_SUBREAPER, 1);
+	while (wait(NULL) > 0) {
+		if (shutdown_requested) {
+			break;
+		}
+	}
+
+	/* Shutdown */
+	int timeout = 1;
+	while (npids() > 1) {
+		if (timeout > PROCESS_TIMEOUT) {
+			kill(-1, SIGKILL);
+			break;
+		}
+		kill(-1, SIGTERM);
+		sleep(1);
+		timeout ++;
+	}
+	sync();
+	sync();
+	reboot(RB_POWER_OFF);
+	return 0;
 }
 
 int main() {
 	if (!isroot()) {
-		syscall(SYS_write, 1, "Error: Not running as root.\n", 27);
+		syscall(SYS_write, 1, NOT_ROOT_ERROR, sizeof(NOT_ROOT_ERROR) - 1);
 		return 1;
 	}
-	init_fs();
-	shutdown();
+	initfs();
+	syscall(SYS_write, 1, PRESYSINIT_MESSAGE, sizeof(PRESYSINIT_MESSAGE) - 1);
+	initsys();
 	return 0;
 }
