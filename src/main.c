d0838015637353ebbd72f3388f52a0e54dfbd06d
Minetomba <147551445+Minetomba@users.noreply.github.com>
2026-09-18 11:16:04 +0300

    12

--- src/main.c
+++ src/main.c
@@ -22,6 +22,10 @@
 #define PRESYSINIT_MESSAGE "System launch!\n"
 #define USERSPACE_INIT_ERROR "Error: Binary cannot have arguments and be ran as the init system at the same time.\n"
 #define CWD_ERROR "Error: Failed to get current directory path.\n"
+#define MAX_ARGS 64
+#define SHELL_PROMPT "> "
+#define BINARY_NOT_FOUND_ERROR "Error: Binary not found."
+#define SHELL_HELP_MESSAGE "exit > Make the shell exit\ncd [path] > Make the shell change directory to that path\npwd > Print the shell's current working directory\nexec [path] [args] > Execute the binary at that absolute path with those arguments\nbg [command] [args] > Run that command with those arguments in the background\nexecbg [path] [args] > Execute the binary at that absolute path in the background with the specified arguments\nhelp > Show this message\n[command] [args] > Run that command with those arguments, and wait for it to finish\n"
 
 /* Signal Handler */
 static volatile sig_atomic_t shutdown_requested = 0;
@@ -171,7 +175,103 @@ int main(int argc, char* argv[]) {
 				closedir(d);
 			}
 			if (strcmp(argv[1], "sh") == 0) {
-				
+				while (1) {
+					write(1, SHELL_PROMPT, sizeof SHELL_PROMPT - 1);
+					char cmd[PATH_MAX];
+					int n = read(STDIN_FILENO, cmd, sizeof cmd - 1);
+					if (n < 1) {
+						_exit(1);
+					}
+					cmd[n] = '\0';
+					if (cmd[0] == 'e' && cmd[1] == 'x' && cmd[2] == 'i' && cmd[3] == 't' && (cmd[4] == ' ' || cmd[4] == 0 || cmd[4] == '\n')) {
+						_exit(0);
+					} else if (cmd[0] == 'c' && cmd[1] == 'd' && cmd[2] == ' ') {
+						char *arg = cmd + 3;
+						arg[strcspn(arg, "\n")] = '\0';
+						chdir(arg);
+					} else if (cmd[0] == 'p' && cmd[1] == 'w' && cmd[2] == 'd' && (cmd[3] == ' ' || cmd[3] == 0 || cmd[3] == '\n')) {
+						char output[PATH_MAX];
+						if (getcwd(output, sizeof(output)) != NULL) {
+							write(1, output, strlen(output));
+							write(1, "\n", 1);
+						} else {
+							write(1, CWD_ERROR, sizeof CWD_ERROR - 1);
+						}
+					} else if (cmd[0] == 'e' && cmd[1] == 'x' && cmd[2] == 'e' && cmd[3] == 'c' && cmd[4] == ' ') {
+						char *argv[MAX_ARGS];
+						int argc = 0;
+						char *tok = strtok(cmd, " \t\n");
+						while (tok && argc < MAX_ARGS - 1) {
+							argv[argc++] = tok;
+							tok = strtok(NULL, " \t\n");
+						}
+						argv[argc] = NULL;
+						for (int j = 0; j < MAX_ARGS - 1; j++) {
+							argv[j] = argv[j + 1];
+						}
+						int pid = fork();
+						if (pid == 0) {
+							execve(argv[0], argv, NULL);
+							write(1, BINARY_NOT_FOUND_ERROR, sizeof BINARY_NOT_FOUND_ERROR - 1);
+							_exit(127);
+						}
+						waitpid(pid, NULL, 0);
+					} else if (cmd[0] == 'b' && cmd[1] == 'g' && cmd[2] == ' ') {
+						char *argv[MAX_ARGS];
+						int argc = 0;
+						char *tok = strtok(cmd, " \t\n");
+						while (tok && argc < MAX_ARGS - 1) {
+							argv[argc++] = tok;
+							tok = strtok(NULL, " \t\n");
+						}
+						argv[argc] = NULL;
+						for (int j = 0; j < MAX_ARGS - 1; j++) {
+							argv[j] = argv[j + 1];
+						}
+						int pid = fork();
+						if (pid == 0) {
+							execvp(argv[0], argv);
+							write(1, BINARY_NOT_FOUND_ERROR, sizeof BINARY_NOT_FOUND_ERROR - 1);
+							_exit(127);
+						}
+					} else if (cmd[0] == 'e' && cmd[1] == 'x' && cmd[2] == 'e' && cmd[3] == 'c' && cmd[4] == 'b' && cmd[5] == 'g' && cmd[6] == ' ') {
+						char *argv[MAX_ARGS];
+						int argc = 0;
+						char *tok = strtok(cmd, " \t\n");
+						while (tok && argc < MAX_ARGS - 1) {
+							argv[argc++] = tok;
+							tok = strtok(NULL, " \t\n");
+						}
+						argv[argc] = NULL;
+						for (int j = 0; j < MAX_ARGS - 1; j++) {
+							argv[j] = argv[j + 1];
+						}
+						int pid = fork();
+						if (pid == 0) {
+							execve(argv[0], argv, NULL);
+							write(1, BINARY_NOT_FOUND_ERROR, sizeof BINARY_NOT_FOUND_ERROR - 1);
+							_exit(127);
+						}
+					} else if (cmd[0] == 'h' && cmd[1] == 'e' && cmd[2] == 'l' && cmd[3] == 'p' && (cmd[4] == ' ' || cmd[4] == 0 || cmd[4] == '\n')) {
+						write(1, SHELL_HELP_MESSAGE, sizeof SHELL_HELP_MESSAGE - 1);
+					} else {
+						char *argv[MAX_ARGS];
+						int argc = 0;
+						char *tok = strtok(cmd, " \t\n");
+						while (tok && argc < MAX_ARGS - 1) {
+							argv[argc++] = tok;
+							tok = strtok(NULL, " \t\n");
+						}
+						argv[argc] = NULL;
+						int pid = fork();
+						if (pid == 0) {
+							execvp(argv[0], argv);
+							write(1, BINARY_NOT_FOUND_ERROR, sizeof BINARY_NOT_FOUND_ERROR - 1);
+							_exit(127);
+						}
+						waitpid(pid, NULL, 0);
+					}
+				}
 			}
 		}
 	}
